63cef0d2ffe914d3cfa447146ab869c1f331d639
markmathiasx <255676778+markmathiasx@users.noreply.github.com>
2026-04-28 01:52:30 -0300

    chore: fix eslint flat config, remove temp test file, clear stale tsbuildinfo

--- _type_test.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-import * as runtime from '@prisma/client/runtime/client.js';
-
-type DenyList = runtime.ITXClientDenyList;
-type TxClient = Omit<PrismaClient, DenyList>;
-const tx: TxClient = {} as TxClient;
-// @ts-expect-error -- If address exists, this will fail (we want it to)
-const x: never = tx.address;
