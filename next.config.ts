6052925a77f7e049a330a6e770906de992c57142
markmathiasx <255676778+markmathiasx@users.noreply.github.com>
2026-05-03 13:22:24 -0300

    Fix Vercel trace excludes

--- next.config.ts
+++ next.config.ts
@@ -67,7 +67,7 @@ const nextConfig: NextConfig = {
   compress: true,
   reactStrictMode: true,
   outputFileTracingExcludes: {
-    "*": [
+    "/**/*": [
       "./public/**/*",
       "./reports/**/*",
       "./assets/**/*",
